2d81ba8f10a2e969f0545a2a155c3697244ff157
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-03 22:22:59 -0400

    add functionality according to README.md to remove certain elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+
+    for(let arg of theArgs){
+        for(i = 0; i < array.length; i++){
+            if(array[i] === arg){
+                array.splice(i, 1);
+                --i;
+            }            
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
