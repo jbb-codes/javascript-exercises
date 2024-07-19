a75f54f7bad06bfe96248106369f612275ea037a
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-19 15:39:23 -0400

    add functionality according to README.md

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  newStr = str.replace(/[\W_]/g, "").toLowerCase();
+  temp = newStr.split("").reverse().join("");
+  return newStr === temp;
 };
 
 // Do not edit below this line
