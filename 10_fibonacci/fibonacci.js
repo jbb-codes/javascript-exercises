81816cfe201f4bbbf10337e9a5b172da23b191d0
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-19 16:47:34 -0400

    Implement functionality according to README.md

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  let n1 = 1;
+  let n2 = 1;
+  let next;
 
+  let count;
+  if (typeof index !== "number") {
+    count = parseInt(index);
+  } else {
+    count = index;
+  }
+
+  if (count === 0) return 0;
+  if (count < 0) return "OOPS";
+
+  for (let i = 1; i < index; i++) {
+    next = n1 + n2;
+    n1 = n2;
+    n2 = next;
+  }
+  return n1;
 };
 
 // Do not edit below this line
