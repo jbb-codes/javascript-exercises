d9ae5342915dff49954a64f068868ea0d023f181
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2024-07-19 19:21:03 -0400

    complete function according to README.md

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, person) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+    return oldestAge > personAge ? oldest : person;
+  });
 };
 
 // Do not edit below this line
